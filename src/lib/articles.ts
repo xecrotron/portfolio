e83b3a4411380809f6489483352069fdbbf3fed0
Lakshay <ljain@webmd.net>
2025-07-08 11:33:24 +0530

    fix: added dynamic import

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -1,24 +1,26 @@
 import type { Article } from "@/types";
-import article from "./articles/[id]/1/page";
-import article2 from "./articles/[id]/2/page";
-import article3 from "./articles/[id]/1/page";
+const articleIds = ["1", "2", "3"];
 
-// In-memory store: To add, edit, or remove an article, modify this array.
-// The slug should be a URL-friendly version of the title.
-let articles: Article[] = [
-  article,
-  article2,
-  article3,
-];
+const allarticles: Promise<Article[]> = Promise.all(
+  articleIds.map((id) => import(`./articles/[id]/${id}/page`).then((mod) => mod.default))
+);
+
+let articles: Article[] 
+async function renderArticles(){
+  articles = await allarticles;
+}
 
 export const getArticles = async (): Promise<Article[]> => {
+  await renderArticles();
   return articles.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
 };
 
 export const getArticleBySlug = async (slug: string): Promise<Article | undefined> => {
+  await renderArticles();
   return articles.find((article) => article.slug === slug);
 };
 
 export const getArticleById = async (id: string): Promise<Article | undefined> => {
+  await renderArticles();
   return articles.find((article) => article.id === id);
 };
