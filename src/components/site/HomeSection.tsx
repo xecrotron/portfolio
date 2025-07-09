cf56e8d3238f6f34db3d28794bd87becdae4dfcc
Lakshay <ljain@webmd.net>
2025-07-09 23:30:00 +0530

    Revert "fix: updated image page for homesection"
    This reverts commit cfc961a87eeccabcc033819cc28aca03d78d46e4.

--- src/components/site/HomeSection.tsx
+++ src/components/site/HomeSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ArrowDown } from "lucide-react";
-import profile from "@/static/media/profile.jpg";
+import profile from "@/public/profile.png";
 
 export function HomeSection() {
   return (
