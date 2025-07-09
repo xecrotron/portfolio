9f336724206746a6cc9bdae9b76dc51fa333e817
Lakshay <ljain@webmd.net>
2025-07-09 23:36:58 +0530

    fix: updated images path

--- src/components/site/HomeSection.tsx
+++ src/components/site/HomeSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ArrowDown } from "lucide-react";
-import profile from "@/public/profile.png";
+import profile from "@/../public/profile.png"; // Adjust the path as necessary";
 
 export function HomeSection() {
   return (
